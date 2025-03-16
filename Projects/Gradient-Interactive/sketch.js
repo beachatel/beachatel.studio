2e771f2dd7fbac417e0ff407a80cfe7c2f3db58a
Bea Chatel <bchatelbell@gmail.com>
2025-03-16 17:10:59 +0000

    Updated Gradient Interactive

--- Projects/Gradient-Interactive/sketch.js
+++ Projects/Gradient-Interactive/sketch.js
@@ -10,11 +10,12 @@ let x1 = 10;
   let r1,r2,r3,r4; 
   let r;
 
-let tanCheckBox,sinCheckBox,check1,check2,check3,check4;
+let tanCheckBox,sinCheckBox,check1,check2,check3,check4,rectCheckBox,rectSinCheckBox;
 let translateSlider,gridSizeSlider,gridSizeMultSlider;  
 let tanEllipseAnimation,sinEllipseAnimation;
 let bgColorPicker;
 let saveImageButton;
+let colorInputs = [];
 
 function setup() {
   createCanvas(windowWidth / 1.25 , windowHeight);
@@ -22,89 +23,151 @@ function setup() {
 //  frameRate(1);
   translateSlider = createSlider(1,20,10,0.01);
     translateSlider.position(10, 10);
-  translateSlider.size(80);
+    translateSlider.style("font-family", "wanpaku-rera, sans-serif");
+  translateSlider.size(80)
+
+  // Label for translateSlider
+  let translateLabel = createDiv("Translate");
+  translateLabel.position(translateSlider.x + translateSlider.width + 20, translateSlider.y);
+  translateLabel.style("font-family", "wanpaku-rera, sans-serif");
+  translateLabel.style("color", "white");
 
   gridSizeSlider = createSlider(10,100,50,1);
     gridSizeSlider.position(10, 40);
+    gridSizeSlider.style("font-family", "wanpaku-rera, sans-serif");
   gridSizeSlider.size(80);
 
-   gridSizeMultSlider = createSlider(1,4,1,1);
+  // Label for gridSizeSlider
+  let gridSizeLabel = createDiv("Grid Size");
+  gridSizeLabel.position(gridSizeSlider.x + gridSizeSlider.width + 20, gridSizeSlider.y);
+  gridSizeLabel.style("font-family", "wanpaku-rera, sans-serif");
+  gridSizeLabel.style("color", "white");
+
+   gridSizeMultSlider = createSlider(1,4,1,0.1);
     gridSizeMultSlider.position(10, 70);
+        gridSizeMultSlider.style("font-family", "wanpaku-rera, sans-serif");
   gridSizeMultSlider.size(80);
+
+  // Label for gridSizeMultSlider
+  let gridSizeMultLabel = createDiv("Grid Mult");
+  gridSizeMultLabel.position(gridSizeMultSlider.x + gridSizeMultSlider.width + 20, gridSizeMultSlider.y);
+  gridSizeMultLabel.style("font-family", "wanpaku-rera, sans-serif");
+  gridSizeMultLabel.style("color", "white");
 
   tanCheckBox = createCheckbox("Tan", false);
    tanCheckBox.position( 10, 100)
+    tanCheckBox.style("font-family", "wanpaku-rera, sans-serif")
     .style("color", "white");
 
   tanEllipseAnimation = createCheckbox("Tan Ellipse Animation", false);
    tanEllipseAnimation.position( 70, 100)
-    .style("color", "white");
+    .style("color", "white")
+.style("font-family", "wanpaku-rera, sans-serif");
 
   sinCheckBox = createCheckbox("Sin", false);
    sinCheckBox.position( 10, 120)
-    .style("color", "white");
+    .style("color", "white")
+    .style("font-family", "wanpaku-rera, sans-serif");
 
     sinEllipseAnimation = createCheckbox("Sin Ellipse Animation", false);
    sinEllipseAnimation.position( 70, 120)
-    .style("color", "white");
- r = random(1);
+    .style("color", "white")
+    .style("font-family", "wanpaku-rera, sans-serif");
+
 
   check1 = createCheckbox("check 1", false);
    check1.position( 10, 140)
-    .style("color", "white");
- r = random(1);
+    .style("color", "white")
+    .style("font-family", "wanpaku-rera, sans-serif");
+
 
   check2 = createCheckbox("check 2", false);
    check2.position( 10, 160)
-    .style("color", "white");
- r = random(1);
+    .style("color", "white")
+    .style("font-family", "wanpaku-rera, sans-serif");
+
 
   check3 = createCheckbox("check 3", false);
    check3.position( 10, 180)
-    .style("color", "white");
- r = random(1);
+    .style("color", "white")
+        .style("font-family", "wanpaku-rera, sans-serif");
+
+
 
   check4 = createCheckbox("check 4", false);
    check4.position( 10, 200)
-    .style("color", "white");
+    .style("color", "white")
+        .style("font-family", "wanpaku-rera, sans-serif");
 
+    rectCheckBox = createCheckbox("Display rectangles ", true);
+   rectCheckBox.position( 90, 200)
+    .style("color", "white")
+        .style("font-family", "wanpaku-rera, sans-serif");
+
+     rectSinCheckBox = createCheckbox("Affect rectangle position with sin", false);
+   rectSinCheckBox.position( 10, 220)
+    .style("color", "white")
+    .style("font-family", "wanpaku-rera, sans-serif");
+
+  
  bgColorPicker = createColorPicker("#000000");
-  bgColorPicker.position(10, 240);
+   bgColorPicker.style("border","none")
+  bgColorPicker.position(10, 280);
   createDiv("Background Color")
-    .position(10, 280)
-    .style("color", "white");
+    .position(10, 260)
 
-  // color pallete picker
+  
+    .style("color", "white")
+    .style("font-family", "wanpaku-rera, sans-serif")
+    .style("border","none");
 
+ 
+ // color pallete picker
 
-  createDiv("Colour Pallette")
-    .position(10, 350)
-    .style("color", "black")
-    .style("padding-bottom","100px")
-    .style("padding-right","150px")
-    .style("background-color","white")
-    
+createDiv("Color Palette")
+  .position(10, 350)
+  .style("color", "white")
+  .style("font-family", "wanpaku-rera, sans-serif");
+
+colorDiv = createDiv("")
+  .position(10, 370)
+  .style("padding", "10px")
+  .style("background-color", "black");
 
     saveImageButton = createButton('Save Image');
   saveImageButton.mousePressed(saveImage);
   saveImageButton.position(10, 700);
-  saveImageButton.style("background-color", "white");
+  saveImageButton.style("background-color", "white")
+  saveImageButton.style("border","none")
+  saveImageButton.style("font-family", "wanpaku-rera, sans-serif");
 
+colorDiv = createDiv("").position(10, 370).style("padding", "10px").style("background-color", "black");
 
+for (let i = 0; i < 5; i++) {
+  let colorInput = createColorPicker(i === 0 ? '#e53723' : i === 1 ? '#3d85d8' : i === 2 ? '#e66e95' : i === 3 ? '#3c8253' : '#e58136');
+  colorInput.style("width", "3vw").style("height", "40px"); // Adjust size of color pickers
+   colorInput.style("font-family", "wanpaku-rera, sans-serif")
+   colorInput.style("border","1px solid black")
+  colorInput.parent(colorDiv);
+  colorInputs.push(colorInput);
+}
+  
 
   r = random(1);
 }
 
 
 function draw() {
-  colors = [color('#e53723'), color('#3d85d8'), color('#e66e95'), color('#3c8253'), color('#e58136')];
+  colors = [];
+
+  colors = colorInputs.map(input => input.color());
 
       let s1 = translateSlider.value();
       let gridSize = gridSizeSlider.value();
       let s2 = gridSizeMultSlider.value();
  
   let baseNoise = frameCount * 0.002;
-  r1 = map(noise(baseNoise), 0, 1, 0.5, 1.2);
+  r1 = map(noise(baseNoise), 0, 1, 0.3, 0.9);
 
   let nOffset = 0.1;
 
@@ -155,7 +218,16 @@ function draw() {
       strokeWeight(r1 * gridSize / 4)
       fill(interD);
       
-      rect(x / sin(2), y / sin(2), gridSize * r1 / 1.3, gridSize * r1 / 1.3);
+
+      if (rectCheckBox.checked()){
+   rect(x, y, gridSize * r1, gridSize * r1);
+      }
+   
+
+   if (rectSinCheckBox.checked()){ // effect rectangles with sin
+ rect(x / sin(2), y / sin(2), gridSize * r1, gridSize * r1);
+   }
+
     noStroke();
       fill(interB);
 
@@ -215,7 +287,7 @@ if (sinCheckBox.checked()) {
  
 
       fill(interC);
-       ellipse(x * sin (2), y * sin(2), gridSize / 2.5, gridSize / 2.5);
+      //  ellipse(x * sin (2), y * sin(2), gridSize / 2.5, gridSize / 2.5);
      
       stroke(interD);
       strokeWeight(2);
