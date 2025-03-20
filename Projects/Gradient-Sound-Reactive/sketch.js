221c190e3e47a26693b11203dd11076bd8a40f50
Bea Chatel <bchatelbell@gmail.com>
2025-03-20 15:09:10 +0000

    Updated Gradient Sound Reactive in Projects

--- Projects/Gradient-Sound-Reactive/sketch.js
+++ Projects/Gradient-Sound-Reactive/sketch.js
@@ -17,7 +17,8 @@ let doCheck1 = false, doCheck2 = false, doCheck3 = false, doCheck4 = false;
 let doRect = true;
 let doRectSin = false;
 let soundFrameRate;
-let dgs;
+let dgs,dgsX,dgsY; //dynamic gridsize for x and y
+let dw,dh; //dynamic width and height in grid for loop
 
 let r1;
 let v1, v2, v3, v4, v5;
@@ -28,10 +29,10 @@ let t = 0;
 
 function preload() {
 
-  sound = loadSound("Data/tambor.mp3");
+  sound = loadSound("Data/chord.mp3");
 }
-
-function setup() {
+  
+  function setup() {
   createCanvas(windowWidth, windowHeight);
 
 //  audioIn = new p5.AudioIn();
@@ -65,7 +66,7 @@ let palette = [
 
 
   colorMode(HSB, 255);
-  t += 0.0005 ;
+  t += 0.005 ;
   let hueVal = map(noise(t), 0, 1, 0, 255);
   background(hueVal, 255, 255);
 
@@ -76,35 +77,57 @@ let palette = [
   let spectrum = fft.analyze();
   let bassEnergy   = fft.getEnergy("bass","30");   // 20–140 Hz
   let midEnergy    = fft.getEnergy("mid");    // 400–2,600 Hz
-  let snare = fft.getEnergy("treble","5000"); // snare
+  let snare = fft.getEnergy("treble","4000"); // snare
   let level = amplitude.getLevel();    // overall amplitude (0..1-ish)
 
 // console.log(bassEnergy);
-  s1       = map(midEnergy,   0, 255,  20,  20);
-  gridSize = map(bassEnergy,    0, 255, 100, 10);
-  s2       = map(snare, 0, 255,  1,   10);
+  s1       = map(midEnergy,   0, 255,  1,8);
+  gridSize = map(bassEnergy,    0, 255, 5, 20);
+  s2       = map(snare, 0, 255,  1,   5);
   mids = map(midEnergy,0,255,10,100);
 
-  soundFrameRate = map(level,0,1,10,60)
+  soundFrameRate = map(level,0,1,10,80)
 
 console.log(soundFrameRate);
 
-  doTan         = (bassEnergy > 20);
-  doSin         = (midEnergy > 100);
+  doTan         = (bassEnergy > 50);
+  doSin         = (midEnergy > 15);
   doTanEllipse  = (level > 0.3);
   doSinEllipse  = (level > 0.2);
-  doCheck1      = (snare > 2);
+  doCheck1      = (snare > 1);
   doCheck2      = (mids > 50);
 
-  if (level < 0.25){
-  dgs = gridSize * s2;
-  } else if (level < 0.5){
-    dgs = 30;
-  } else if (level < 0.75){
-    dgs = 100;
-  } else if (level < 1){
-    dgs = 200;
+  //   dgs = gridSize * s2;
+
+  // dgs = gridSize;
+
+ if (level < 0.5){
+  dgsX = 50;
+ } else if (level > 0.5){
+ dgsX = gridSize * s2;
   }
+
+  if (level > 0.5){
+ dgsY = 50
+  } else if (level < 0.5){
+    dgsY = gridSize * s2;
+  }
+
+   if (level > 0.7){
+  dw = width/level;
+ } else if (level < 0.5){
+ dw= width;
+  }
+
+//   if (level > 0.5){
+//  dh = height;
+//   } else if (level < 0.5){
+//     dh = height/2;
+//   }
+dh = height;
+
+
+ 
 
   // doCheck3     = (snare > 100);
   // doCheck4     = (snare > 100);
@@ -112,7 +135,7 @@ console.log(soundFrameRate);
 
   //rect size
   let baseNoise = frameCount * 0.005; 
-  r1 = map(noise(baseNoise), 0, 1, 0.3, 2);
+  r1 = map(noise(baseNoise), 0, 1, 0.3, 1);
 
 
   v1 = abs(sin(frameCount * 0.005));
@@ -123,7 +146,7 @@ console.log(soundFrameRate);
 
 
   let fromIndex = floor(noise(baseNoise) * palette.length);
-  let toIndex   = floor(noise(baseNoise + 10) * palette.length);
+  let toIndex   = floor(noise(baseNoise + 100) * palette.length);
   let fromColor = color(palette[fromIndex]);
   let toColor   = color(palette[toIndex]);
 
@@ -142,10 +165,10 @@ console.log(soundFrameRate);
   ellipseMode(CENTER);
 
 
-  for (let x = 0; x < width; x += 100) { //dynamic gridsize update
-    for (let y = 0; y < height; y += dgs) {
+  for (let x = 0; x < dw; x += dgsX) { //dynamic gridsize update
+    for (let y = 0; y < dh; y += dgsY) {
    
-      // x += trebleEnergy / 1000;
+
   
       push();
    
@@ -182,15 +205,15 @@ console.log(soundFrameRate);
         ellipse(x, y, gridSize * tan(1) * e1, gridSize * tan(1) * e1);
       }
       if (doSinEllipse) {
-        let e2 = 1 - (level * 2.0);
+        let e2 = 1 - (level * 5.0);
         ellipse(x, y, gridSize * sin(1) * e2, gridSize * sin(1) * e2);
       }
 
   
       if (doCheck1) {
-        if(level < 0.4 && snare > 8){
+        if(level < 0.2){
     ellipse(x * sin(2), y / sin(2), gridSize * bassEnergy / 2, gridSize);
-        } else if(level > 0.4 ){
+        } else if(level > 0.2 ){
          
            ellipse(x * sin(2), y / sin(2), gridSize, gridSize * bassEnergy / 2);
         }
