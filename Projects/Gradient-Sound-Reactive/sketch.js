7e45379fc8453712f6d63cc5fde04879180b5496
Bea Chatel <bchatelbell@gmail.com>
2025-03-20 12:20:55 +0000

    Updated Gradient Sound Reactive

--- Projects/Gradient-Sound-Reactive/sketch.js
+++ Projects/Gradient-Sound-Reactive/sketch.js
@@ -3,6 +3,7 @@
 let sound;
 let fft;
 let amplitude;
+let audioIn;
 
 
 let s1;       
@@ -15,6 +16,8 @@ let doSinEllipse = false;
 let doCheck1 = false, doCheck2 = false, doCheck3 = false, doCheck4 = false;
 let doRect = true;
 let doRectSin = false;
+let soundFrameRate;
+let dgs;
 
 let r1;
 let v1, v2, v3, v4, v5;
@@ -31,13 +34,17 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // Make the animation slower
-  frameRate(10);
+//  audioIn = new p5.AudioIn();
+
+//   // start the Audio Input.
+//   // By default, it does not .connect() (to the computer speakers)
+//   audioIn.start();
+  
 
   fft = new p5.FFT();
   amplitude = new p5.Amplitude();
 
-  sound.play();
+
   sound.loop();
 
 
@@ -46,6 +53,7 @@ function setup() {
 
 function draw() {
 
+frameRate(soundFrameRate);
 
 let palette = [
   "#e53723",
@@ -57,7 +65,7 @@ let palette = [
 
 
   colorMode(HSB, 255);
-  t += 0.005 ;
+  t += 0.0005 ;
   let hueVal = map(noise(t), 0, 1, 0, 255);
   background(hueVal, 255, 255);
 
@@ -66,27 +74,45 @@ let palette = [
 
 
   let spectrum = fft.analyze();
-  let bassEnergy   = fft.getEnergy("bass");   // 20–140 Hz
+  let bassEnergy   = fft.getEnergy("bass","30");   // 20–140 Hz
   let midEnergy    = fft.getEnergy("mid");    // 400–2,600 Hz
-  let trebleEnergy = fft.getEnergy("treble"); // 2,600–20,000 Hz
+  let snare = fft.getEnergy("treble","5000"); // snare
   let level = amplitude.getLevel();    // overall amplitude (0..1-ish)
 
 // console.log(bassEnergy);
-  s1       = map(midEnergy,   0, 255,  20,  50);
-  gridSize = map(bassEnergy,    0, 255, 10, 50);
-  s2       = map(trebleEnergy, 0, 255,  1,   10);
+  s1       = map(midEnergy,   0, 255,  20,  20);
+  gridSize = map(bassEnergy,    0, 255, 100, 10);
+  s2       = map(snare, 0, 255,  1,   10);
+  mids = map(midEnergy,0,255,10,100);
 
+  soundFrameRate = map(level,0,1,10,60)
 
-  doTan         = (bassEnergy > 50);
+console.log(soundFrameRate);
+
+  doTan         = (bassEnergy > 20);
   doSin         = (midEnergy > 100);
   doTanEllipse  = (level > 0.3);
   doSinEllipse  = (level > 0.2);
-  doCheck1      = (trebleEnergy > 50);
+  doCheck1      = (snare > 2);
+  doCheck2      = (mids > 50);
+
+  if (level < 0.25){
+  dgs = gridSize * s2;
+  } else if (level < 0.5){
+    dgs = 30;
+  } else if (level < 0.75){
+    dgs = 100;
+  } else if (level < 1){
+    dgs = 200;
+  }
+
+  // doCheck3     = (snare > 100);
+  // doCheck4     = (snare > 100);
 
 
-
+  //rect size
   let baseNoise = frameCount * 0.005; 
-  r1 = map(noise(baseNoise), 0, 1, 0.3, 1.5);
+  r1 = map(noise(baseNoise), 0, 1, 0.3, 2);
 
 
   v1 = abs(sin(frameCount * 0.005));
@@ -115,11 +141,11 @@ let palette = [
   rectMode(CENTER);
   ellipseMode(CENTER);
 
-  // 6) Draw the grid
-  for (let x = 0; x < width; x += gridSize * s2) {
-    for (let y = 0; y < height; y += gridSize * s2) {
+
+  for (let x = 0; x < width; x += 100) { //dynamic gridsize update
+    for (let y = 0; y < height; y += dgs) {
    
-      // x += trebleEnergy / 2000;
+      // x += trebleEnergy / 1000;
   
       push();
    
@@ -152,7 +178,7 @@ let palette = [
 
       // Animate circles if triggered
       if (doTanEllipse) {
-        let e1 = 1 - (level * 1.0);
+        let e1 = 1 - (level * 5.0);
         ellipse(x, y, gridSize * tan(1) * e1, gridSize * tan(1) * e1);
       }
       if (doSinEllipse) {
@@ -162,9 +188,19 @@ let palette = [
 
   
       if (doCheck1) {
-        ellipse(x * sin(2), y / sin(2), gridSize, gridSize);
+        if(level < 0.4 && snare > 8){
+    ellipse(x * sin(2), y / sin(2), gridSize * bassEnergy / 2, gridSize);
+        } else if(level > 0.4 ){
+         
+           ellipse(x * sin(2), y / sin(2), gridSize, gridSize * bassEnergy / 2);
+        }
+      
       } else if (doCheck2) {
-        ellipse(x / sin(2), y * sin(2), gridSize, gridSize);
+        
+ ellipse(x / sin(2), y * sin(2), gridSize * midEnergy, gridSize * midEnergy);
+        
+       
+
       } else if (doCheck3) {
         ellipse(x / sin(2), y * sin(2), gridSize, gridSize);
       } else if (doCheck4) {
